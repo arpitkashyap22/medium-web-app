001f61af628349cebe555366162f6070e0bef7b3
arpitkashyap22 <arpitkashyap2000@gmail.com>
2024-09-21 22:49:49 +0530

    fetch blogs  implement

--- medium-frontend/src/pages/Blogs.tsx
+++ medium-frontend/src/pages/Blogs.tsx
@@ -4,20 +4,35 @@ import { useBlog } from "../hooks";
 
 export const Blogs = () => {
     const {loading, blogs} = useBlog();
-    console.log(blogs);
+    blogs.forEach(ele => console.log(ele.title));
 
     if(loading){
         return <div>
             loading...
         </div>
     }
+ 
+
   return (
     <div>
       <AppBar></AppBar>
       <div className="flex justify-center">
-        <div className="max-w-xl">
-            <BlogCard authorName="Arpit kashyap" title="AI Sustainablity" content="TEch park" publishedDate={"22-12-2000"}></BlogCard>
-        </div>
+      {blogs.length > 0 ? (
+          blogs.map((blog) => (
+            <BlogCard
+              // Ensure unique key for each BlogCard
+              authorName={blog.authorId}
+              title={blog.title}
+              content={blog.content}
+              publishedDate={blog.publishedDate}
+            />
+          ))
+        ) : (
+          <p>No blogs available</p>  // Handle empty blogs case
+        )}
+        {/* <div className="max-w-xl">
+           <BlogCard authorName="Arpit kashyap" title="AI Sustainablity" content="TEch park" publishedDate={"22-12-2000"}></BlogCard>
+        </div> */}
       </div>
     </div>
   );
